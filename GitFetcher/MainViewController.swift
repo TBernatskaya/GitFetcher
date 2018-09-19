abd490fa0666173f6ee728c4ff435bf9e7cbc80a
Tetiana Bernatska <tetiana.bernatska@comhem.com>
2018-09-19 18:31:50 +0200

    Display fetched data in ui

--- GitFetcher/MainViewController.swift
+++ GitFetcher/MainViewController.swift
@@ -12,18 +12,42 @@ class MainViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var searchResultsTableView: UITableView!
+    @IBOutlet weak var errorLabel: UILabel!
     
     let apiService = ApiService.init()
+    
+    var repositoriesList: [Repository]? {
+        didSet {
+            updateView()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Search"
         
+        registerClasses()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        self.searchBar.becomeFirstResponder()
+        searchBar.becomeFirstResponder()
+    }
+    
+    fileprivate func updateView() {
+        if repositoriesList?.count == 0 {
+            searchResultsTableView.isHidden = true
+            errorLabel.isHidden = false
+        } else {
+            errorLabel.isHidden = true
+            searchResultsTableView.reloadData()
+        }
+    }
+    
+    fileprivate func registerClasses() {
+        let cellReuseIdentifier = "searchResultCell"
+        let cellNib = UINib.init(nibName: "SearchResultTableViewCell", bundle: Bundle.main)
+        searchResultsTableView.register(cellNib, forCellReuseIdentifier: cellReuseIdentifier)
     }
 }
 
@@ -33,7 +57,13 @@ extension MainViewController: UISearchBarDelegate {
         guard let searchText = searchBar.text else { return }
         
         apiService.repositories(for: searchText, completion: { repositories, error in
-            print(repositories)
+            if let repositories = repositories {
+                self.repositoriesList = repositories
+            } else if let error = error {
+                self.searchResultsTableView.isHidden = true
+                self.errorLabel.text = error.localizedDescription
+                self.errorLabel.isHidden = false
+            }
         })
     }
 }
@@ -45,12 +75,18 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return repositoriesList?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = searchResultsTableView.dequeueReusableCell(withIdentifier: "searchResultCell")
+        let cell = searchResultsTableView.dequeueReusableCell(withIdentifier: "searchResultCell", for: indexPath) as! SearchResultTableViewCell
         
-        return cell!
+        guard let repositoriesList = self.repositoriesList else { return cell }
+        cell.nameLabel.text = repositoriesList[indexPath.row].name
+        cell.descriptionLabel.text = repositoriesList[indexPath.row].description
+        cell.numberOfWatchersLabel.text = String(repositoriesList[indexPath.row].numberOfWatchers)
+        cell.urlLabel.text = repositoriesList[indexPath.row].url.absoluteString
+        
+        return cell
     }
 }
