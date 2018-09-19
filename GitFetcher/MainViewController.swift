0774ea80d78d4d2a35cf40f6c093b59e488bd614
Tetiana Bernatska <tetiana.bernatska@comhem.com>
2018-09-19 20:21:19 +0200

    Reorganized project structure

--- GitFetcher/MainViewController.swift
+++ GitFetcher/MainViewController.swift
@@ -31,6 +31,10 @@ class MainViewController: UIViewController {
         super.viewDidAppear(animated)
         searchBar.becomeFirstResponder()
     }
+    
+    func clearView() {
+        repositoriesList?.removeAll()
+    }
 }
 
 fileprivate extension MainViewController {
@@ -57,62 +61,9 @@ fileprivate extension MainViewController {
         errorLabel.isHidden = false
     }
     
-    func clearView() {
-        repositoriesList?.removeAll()
-    }
-    
     func registerClasses() {
         let cellReuseIdentifier = "searchResultCell"
         let cellNib = UINib.init(nibName: "SearchResultTableViewCell", bundle: Bundle.main)
         searchResultsTableView.register(cellNib, forCellReuseIdentifier: cellReuseIdentifier)
-    }
-}
-
-extension MainViewController: UISearchBarDelegate {
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let searchText = searchBar.text else { return }
-        
-        ApiService().repositories(for: searchText, completion: { repositories, error in
-            if let repositories = repositories {
-                self.repositoriesList = repositories
-            } else if let error = error {
-                self.searchResultsTableView.isHidden = true
-                self.errorLabel.text = error.localizedDescription
-                self.errorLabel.isHidden = false
-            }
-        })
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText == "" {
-            clearView()
-        }
-    }
-    
-    func searchBarCancelButtonClicked(_ seachBar: UISearchBar) {
-        clearView()
-    }
-}
-
-extension MainViewController: UITableViewDataSource {
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repositoriesList?.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = searchResultsTableView.dequeueReusableCell(withIdentifier: "searchResultCell", for: indexPath) as! SearchResultTableViewCell
-        
-        guard let repositoriesList = self.repositoriesList else { return cell }
-        cell.nameLabel.text = repositoriesList[indexPath.row].name
-        cell.descriptionLabel.text = repositoriesList[indexPath.row].description
-        cell.urlLabel.text = repositoriesList[indexPath.row].url.absoluteString
-        
-        return cell
     }
 }
