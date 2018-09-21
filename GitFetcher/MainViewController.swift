ce9c98617d9cdf2e49611fe6ab1c77f076f523a2
Tetiana Bernatska <tetiana.bernatska@comhem.com>
2018-09-21 17:39:53 +0200

    Recalculate tableview frame when show hide keyboard

--- GitFetcher/MainViewController.swift
+++ GitFetcher/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var searchResultsTableView: UITableView!
     @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var searchResultsTableViewBottom: NSLayoutConstraint!
     
     var repositoriesList: [Repository]? {
         didSet {
@@ -28,9 +29,19 @@ class MainViewController: UIViewController {
         registerClasses()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addObservers()
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         searchBar.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeObservers()
     }
     
     func clearView() {
@@ -72,5 +83,38 @@ fileprivate extension MainViewController {
         let cellReuseIdentifier = "searchResultCell"
         let cellNib = UINib.init(nibName: "SearchResultTableViewCell", bundle: Bundle.main)
         searchResultsTableView.register(cellNib, forCellReuseIdentifier: cellReuseIdentifier)
+    }
+    
+    func addObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
+    func removeObservers() {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: nil)
+        
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillHideNotification,
+                                                  object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let keyboardHeigt = keyboardFrame.height
+        
+        searchResultsTableViewBottom.constant = keyboardHeigt
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        searchResultsTableViewBottom.constant = 20
     }
 }
