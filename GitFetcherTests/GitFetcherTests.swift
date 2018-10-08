ffcb1b46ec0f52ed2f5cbcd669543be081943b9f
Tetiana Bernatska <tetiana.bernatska@comhem.com>
2018-10-08 21:54:55 +0200

    Added new unit tests

--- GitFetcherTests/GitFetcherTests.swift
+++ GitFetcherTests/GitFetcherTests.swift
@@ -14,6 +14,7 @@ class GitFetcherTests: XCTestCase {
     
     let host = "api.github.com"
     lazy var testBundle = Bundle(for: type(of: self))
+    let notConnectedError = NSError(domain:NSURLErrorDomain, code:Int(CFNetworkErrors.cfurlErrorNotConnectedToInternet.rawValue), userInfo:nil)
 
     override func tearDown() {
         super.tearDown()
@@ -78,18 +79,97 @@ class GitFetcherTests: XCTestCase {
         wait(for: [expectation], timeout: 10.0)
     }
     
-    func testReceivedError() {
+    func testGitUserReceivedError() {
         let apiService = ApiService.init()
         let expectation = XCTestExpectation.init(description: "Waiting for response")
         let notExistingUserName = "blah123"
         
         stub(condition: isHost(self.host)) { _ in
-            let notConnectedError = NSError(domain:NSURLErrorDomain, code:Int(CFNetworkErrors.cfurlErrorNotConnectedToInternet.rawValue), userInfo:nil)
-            return OHHTTPStubsResponse(error:notConnectedError)
+            return OHHTTPStubsResponse(error:self.notConnectedError)
         }
         
         apiService.repositories(for: notExistingUserName, completion: { repos, error in
             XCTAssertNil(repos)
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testStarRepository() {
+        let apiService = ApiService.init()
+        let expectation = XCTestExpectation.init(description: "Waiting for response")
+        let existingUserName = "TBernatskaya"
+        let existingRepositoryName = "GitFetcher"
+        
+        stub(condition: isHost(self.host)) { _ in
+            let response = OHHTTPStubsResponse.init()
+            response.statusCode = 204
+            return response
+        }
+        
+        apiService.star(repository: existingRepositoryName, owner: existingUserName, completion: { succeeded, error in
+            XCTAssertTrue(succeeded)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testStarRepositoryReceivedError() {
+        let apiService = ApiService.init()
+        let expectation = XCTestExpectation.init(description: "Waiting for response")
+        let existingUserName = "TBernatskaya"
+        let existingRepositoryName = "GitFetcher"
+        
+        stub(condition: isHost(self.host)) { _ in
+            return OHHTTPStubsResponse(error:self.notConnectedError)
+        }
+        
+        apiService.star(repository: existingRepositoryName, owner: existingUserName, completion: { succeeded, error in
+            XCTAssertFalse(succeeded)
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testUnStarRepository() {
+        let apiService = ApiService.init()
+        let expectation = XCTestExpectation.init(description: "Waiting for response")
+        let existingUserName = "TBernatskaya"
+        let existingRepositoryName = "GitFetcher"
+        
+        stub(condition: isHost(self.host)) { _ in
+            let response = OHHTTPStubsResponse.init()
+            response.statusCode = 204
+            return response
+        }
+        
+        apiService.unstar(repository: existingRepositoryName, owner: existingUserName, completion: { succeeded, error in
+            XCTAssertTrue(succeeded)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        })
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func testUnStarRepositoryReceivedError() {
+        let apiService = ApiService.init()
+        let expectation = XCTestExpectation.init(description: "Waiting for response")
+        let existingUserName = "TBernatskaya"
+        let existingRepositoryName = "GitFetcher"
+        
+        stub(condition: isHost(self.host)) { _ in
+            return OHHTTPStubsResponse(error:self.notConnectedError)
+        }
+        
+        apiService.unstar(repository: existingRepositoryName, owner: existingUserName, completion: { succeeded, error in
+            XCTAssertFalse(succeeded)
             XCTAssertNotNil(error)
             expectation.fulfill()
         })
